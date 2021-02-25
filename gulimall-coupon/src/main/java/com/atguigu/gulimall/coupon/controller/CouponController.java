57a7dd30bb05f1dda044fffaa0d56abc29afe421
daniubi <804703017@qq.com>
2021-02-25 16:25:36 +0800

    ddd

--- gulimall-coupon/src/main/java/com/atguigu/gulimall/coupon/controller/CouponController.java
+++ gulimall-coupon/src/main/java/com/atguigu/gulimall/coupon/controller/CouponController.java
@@ -5,6 +5,8 @@ import java.util.Map;
 
 //import org.apache.shiro.authz.annotation.RequiresPermissions;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -25,12 +27,27 @@ import com.atguigu.common.utils.R;
  * @email 804703017@gmail.com
  * @date 2021-02-18 11:21:02
  */
+@RefreshScope
 @RestController
 @RequestMapping("coupon/coupon")
 public class CouponController {
     @Autowired
     private CouponService couponService;
 
+
+    @Value("${coupon.user.name}")
+    private String name;
+
+
+    @Value("${coupon.user.age}")
+    private Integer age;
+
+    @RequestMapping("/test")
+    public R test(){
+
+        return R.ok().put("name", name).put("age",age);
+
+    }
 
 
     @RequestMapping("member/list")
